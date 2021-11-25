5d7c6ba75a79d6d5613a0eb8d8331394e6acde32
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-11-25 15:12:52 +0000

    feat: displaying messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,30 @@
 const socket = io()
+const chatForm = document.getElementById('chat-form')
 
 socket.on('message', message => {
     console.log(message)
-
+    outPutMessage(message)
 })
+
+// Message submit
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // Get message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg)
+} )
+
+// output message to dom
+const outPutMessage = (message) => {
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`
+    document.querySelector('.chat-messages').appendChild(div)
+}
